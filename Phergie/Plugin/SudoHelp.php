f2e72948503b18f45d54b25d10a0dfad3f4fc44c
Richard Hoppes <rhoppes@zappos.com>
2012-10-03 22:53:04 -0700

    fixed some small bugs in sudohelp plugin

--- Phergie/Plugin/SudoHelp.php
+++ Phergie/Plugin/SudoHelp.php
@@ -26,16 +26,18 @@ class Phergie_Plugin_SudoHelp extends Phergie_Plugin_Abstract
 	}
 
 	public function onCommandHelp() {
+		$connection = $this->getConnection();
+		$botNick = $connection->getNick();
+
+		$prefix = $this->getConfig('command.prefix');
 		$event = $this->getEvent();
 		$input = $event->getArgument(1);
-		$nick = $event->getNick();
 		$source = $event->getSource();
 		$helpMessages = $this->getHelpMessages();
 
 		$pattern = '/'.preg_quote($prefix).'\s*?help '.preg_quote($botNick).'/i';
 		if (preg_match($pattern, $input)) {
 			foreach ($this->plugins as $plugin) {
-				$class = new ReflectionClass($plugin);
 				if(isset($helpMessages[$plugin->getName()])) {
 					$this->doPrivmsg($source, $helpMessages[$plugin->getName()]);
 				}
